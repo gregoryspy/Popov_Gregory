b810b04b9626d5c240371358a4e7554c227ae6f9
Gregory <gg286636@gmail.com>
2021-10-29 11:54:31 +0300

    Chatbot{4-th} stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -9,6 +9,14 @@ w = input ("Second:")
 e = input ("Third:")
 age = ((int(q) / 3 * 70 + int(w) / 5 *21 + int(e) / 7 * 15)%105)
 print ("Your age is " + str(age) + "; that a good time to start programming!")
+number = float (input ("Now i will prove to you that i can count to any number you want:") )
+i = 0
+while i < 10:
+    print(str(i) + "!")
+    i = i + 1
+    if i == number :
+        break
+print("Completed, have a nice day!")
 
 
 
