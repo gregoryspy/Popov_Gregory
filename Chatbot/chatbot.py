b7266ccfaff9d79fc8da2376c393b830436722f3
Gregory <gg286636@gmail.com>
2021-10-29 11:53:28 +0300

    Chatbot{3-rd} stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -2,6 +2,13 @@ print ("Hello! my name is John")
 print ("I was created in 2021")
 what = input ("Please, remind my your name:")
 print ("What a great name you have," + str(what))
+print ("Let me guess your age.")
+print("Enter remainders of dividing your age by 3,5 and 7")
+q = input ("First:")
+w = input ("Second:")
+e = input ("Third:")
+age = ((int(q) / 3 * 70 + int(w) / 5 *21 + int(e) / 7 * 15)%105)
+print ("Your age is " + str(age) + "; that a good time to start programming!")
 
 
 
