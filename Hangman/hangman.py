cd49ff001b4222fa3e97641db8852145e2d1aaa9
gregoryspy <gg286636@gmail.com>
2021-11-06 17:25:38 +0200

    {8-th} stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,24 +1,8 @@
-import random
-print("HANGMAN")
-print("The game will be available soon.")
-words = ['python', 'javascript', 'html', 'java']
-start = 0
-letters = list("qwertyuiopasdfghjklzxcvbnm")
-while (start != 'exit'):
-    if start == "play":
-        count = 0
-        word = random.choice(words)  # python
-        word_list = list(word)  # [p,y,t,h,o,n]
-        user_word = "-" * len(word)  # ------
-        user_used = []    # [-,-,-,-,-,-]
-        user_list = list(user_word)
-        print(user_word)
-        while (count != 8):
-            count += 1
-            answer = str(input('Input a letter:'))
-            if answer in user_used:
-                print("You've already guessed this letter")
-                count -= 1
-                print(''.join(user_list))
-                continue
-            user_used.append(answer)
+
+        elif:
+            print("You lost!")
+        start = str(input('Type "play" to play the game, "exit" to quit:'))
+    elif start == "exit":
+        break
+    else:
+        start = str(input('Type "play" to play the game, "exit" to quit:'))
