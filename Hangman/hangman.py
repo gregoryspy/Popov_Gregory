5def259a0168ff23169dce0138efda4246a33adc
gregoryspy <gg286636@gmail.com>
2021-11-06 17:23:27 +0200

    {6-th} stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -16,3 +16,30 @@ while (start != 'exit'):
         while (count != 8):
             count += 1
             answer = str(input('Input a letter:'))
+            if answer in user_used:
+                print("You've already guessed this letter")
+                count -= 1
+                print(''.join(user_list))
+                continue
+            user_used.append(answer)
+            if answer in word_list:
+                if word_list.count(answer) >= 2:
+                    index = word_list.index(answer)
+                    user_list[index] = answer
+                    word_list[index] = '-'
+                index = word_list.index(answer)
+                user_list[index] = answer
+                count -= 1
+            elif answer not in letters:
+                print("Please enter a lowercase English letter")
+                count -= 1
+                continue
+            elif len(answer) >= 2:
+                print("You should input a single letter")
+                count -= 1
+                continue
+            elif answer not in (word and word_list):
+                print("That letter doesn't appear in the word")
+            print(''.join(user_list))
+        word_list = list(word)
+  
