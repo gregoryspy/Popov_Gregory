650e500fe2195f9535e9eee95dcd6865804874b3
gregoryspy <gg286636@gmail.com>
2021-11-07 22:54:20 +0200

    {5-th} stage

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -1,63 +1,25 @@
 
-def buy():
-    global money, water, milk, beans, cups
 
-    flavor = select_flavor()
+def main():
+    while True:
+        action = select_action()
 
-    try:
-        if flavor == 0:
-            pass
-        elif flavor == 1:  # espresso
-            is_enough(need_water=250, need_beans=16)
-            print("You choosed espresso, it will coast 4 UAH")
-
-            money += 4
-            water -= 250
-            beans -= 16
-            cups -= 1
-        elif flavor == 2:  # latte
-            is_enough(need_water=350, need_milk=75, need_beans=20)
-            print("You choosed espresso, it will coast 7 UAH")
-            money += 7
-            water -= 350
-            milk -= 75
-            beans -= 20
-            cups -= 1
-        elif flavor == 3:  # cappuccino
-            is_enough(need_water=200, need_milk=100, need_beans=12)
-            print("You choosed espresso, it will coast 6 UAH")
-
-            money += 6
-            water -= 200
-            milk -= 100
-            beans -= 12
-            cups -= 1
+        if action == 'buy':
+            buy()
+        elif action == 'fill':
+            fill()
+        elif action == 'take':
+            take()
+        elif action == 'exit':
+            break
+        elif action == 'remaining':
+            print_state()
         else:
-            raise ValueError(f'Unknown flavor {flavor}')
-    except ResourceError:
-        pass
+            raise ValueError(f'Unknown command {action}')
 
 
-def fill():
-    global water, milk, beans, cups
+if __name__ == '__main__':
+    main()
 
-    print()
-    water += int(input('Write how many ml of water do you want to add: '))
-    milk += int(input('Write how many ml of milk do you want to add: '))
-    beans += int(input('Write how many grams of coffee beans'
-                       ' do you want to add: '))
-    cups += int(input('Write how many disposable cups of coffee'
-                      ' do you want to add: '))
-    print()
-
-
-def take():
-    global money
-
-    print()
-    print(f'I gave you ${money}')
-    print()
-
-    money = 0
 
 
