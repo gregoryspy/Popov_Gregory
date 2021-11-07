0b8ecf0ba7f3490fca3a06c9eb3c95a099163dca
gregoryspy <gg286636@gmail.com>
2021-11-07 22:16:16 +0200

    {1-st} stage

---
+++ CoffeMachine/CoffeMachine.py
@@ -0,0 +1,13 @@
+
+    else:
+        print('Starting to make a coffee\n')
+        print('Grinding coffee beans\n')
+        print('Boiling water!\n')
+        print('Mixing boiled water with crushed coffee beans\n')
+        print('Pouring coffee into the cup\n')
+        print('Pouring some milk into the cup\n')
+        print('Coffee is ready!\n')
+
+
+
+
